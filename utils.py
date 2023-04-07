4a2daeefb3ba637ab6b99db61efb726e54af9254
willxxy <90741489+willxxy@users.noreply.github.com>
2023-04-07 12:33:39 -0700

    basic tools

---
+++ utils.py
@@ -0,0 +1,49 @@
+import os
+import json
+import openai
+import tiktoken
+import time
+import backoff
+import random
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.encoding_for_model(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+  
+  
+  
+def retry_with_exponential_backoff(
+    func,
+    initial_delay: float = 1,
+    exponential_base: float = 2,
+    jitter: bool = True,
+    max_retries: int = 10,
+    errors: tuple = (openai.error.RateLimitError,openai.error.APIError),
+):
+    def wrapper(*args, **kwargs):
+        num_retries = 0
+        delay = initial_delay
+
+        while True:
+            try:
+                return func(*args, **kwargs)
+
+            except errors as e:
+                num_retries += 1
+
+                if num_retries > max_retries:
+                    raise Exception(
+                        f"Maximum number of retries ({max_retries}) exceeded."
+                    )
+                delay =1
+                time.sleep(delay)
+
+            except Exception as e:
+                raise e
+    return wrapper
+
+@retry_with_exponential_backoff
+def completions_with_backoff(**kwargs):
+    return openai.ChatCompletion.create(**kwargs)
